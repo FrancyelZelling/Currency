d04bbe2e8578b5cbb61296b5e528ffdbfef8f209
FrancyelZelling <zellingf@gmail.com>
2024-11-01 06:14:38 -0300

    ExchangeApi related classes created

---
+++ src/com/zelling/models/ExchangeApiResponse.java
@@ -0,0 +1,7 @@
+package com.zelling.models;
+
+public class ExchangeApiResponse {
+    private String result;
+    private String baseCode;
+    private NestedExchangeApiResponse nested;
+}
---
+++ src/com/zelling/models/NestedExchangeApiResponse.java
@@ -0,0 +1,6 @@
+package com.zelling.models;
+
+public class NestedExchangeApiResponse {
+    private float usd;
+    private float brl;
+}
