12a27ee846adf324b9cc7893cfebe20d73e98654
FrancyelZelling <zellingf@gmail.com>
2024-11-04 07:45:12 -0300

    Created UiHelper

---
+++ src/com/zelling/tools/UiHelper.java
@@ -0,0 +1,16 @@
+package com.zelling.tools;
+
+public class UiHelper {
+    public void mainMenu(){
+        System.out.println(
+                """
+                 *************************************************       
+                 Bem vindo ao conversor de moedas, selecione a opção desejada:
+                 1) converter valor
+                 2) mostrar histórico de conversões
+                 3) sair
+                 *************************************************       
+                """
+        );
+    }
+}
